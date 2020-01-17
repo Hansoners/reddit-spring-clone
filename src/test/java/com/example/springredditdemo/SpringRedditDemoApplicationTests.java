083b31248ad6e2ce9882e40975d17f796089eeee
hansoners <hansonerschen@gmail.com>
2020-01-16 21:32:28 -0500

    Initial commit

---
+++ src/test/java/com/example/springredditdemo/SpringRedditDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredditdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5cb6dfc82cc556e70947b162f7b5b19b3f8f6241
hansoners <hansonerschen@gmail.com>
2020-02-17 21:28:19 -0500

    Added authorization for subreddit requests.

---
+++ src/main/java/com/example/springredditdemo/controller/SubredditController.java
@@ -0,0 +1,35 @@
+package com.example.springredditdemo.controller;
+
+import com.example.springredditdemo.dto.SubredditDto;
+import com.example.springredditdemo.service.SubredditService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subreddit")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubredditController {
+
+    private final SubredditService subredditService;
+
+    @GetMapping
+    public List<SubredditDto> getAllSubreddits() {
+        return subredditService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public SubredditDto getSubreddit(@PathVariable Long id) {
+        return subredditService.getSubreddit(id);
+    }
+
+    @PostMapping
+    public SubredditDto create(@RequestBody @Valid SubredditDto subreddit) {
+        return subredditService.save(subreddit);
+    }
+
+}

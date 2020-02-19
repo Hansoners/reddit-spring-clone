8c295360b38cb0b3d677518b32c356f5f52ed327
hansoners <hansonerschen@gmail.com>
2020-02-18 23:08:44 -0500

    Added post mapper and post controller.

--- src/main/java/com/example/springredditdemo/controller/PostController.java
+++ src/main/java/com/example/springredditdemo/controller/PostController.java
@@ -1,4 +1,16 @@
 package com.example.springredditdemo.controller;
 
+import com.example.springredditdemo.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts/")
+@AllArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+
 }

3077a98c418dec90b0391afff2394403315b3344
hansoners <hansonerschen@gmail.com>
2020-02-23 22:22:09 -0500

    Modified post controller.

--- src/main/java/com/example/springredditdemo/controller/PostController.java
+++ src/main/java/com/example/springredditdemo/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.example.springredditdemo.controller;
 
+import com.example.springredditdemo.dto.PostRequest;
+import com.example.springredditdemo.dto.PostResponse;
 import com.example.springredditdemo.service.PostService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/api/posts/")
@@ -12,5 +19,25 @@ public class PostController {
 
     private final PostService postService;
 
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) {
+        postService.save(postRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long id) {
+        return status(HttpStatus.OK).body(postService.getPost(id));
+    }
+
+    @GetMapping("by-subreddit/{id}")
+    public ResponseEntity<List<PostResponse>> getPostsBySubreddit(@PathVariable Long id) {
+        return status(HttpStatus.OK).body(postService.getPostsBySubreddit(id));
+    }
+
+    @GetMapping("by-user/{username}")
+    public ResponseEntity<List<PostResponse>> getPostsByUser(@PathVariable String username) {
+        return status(HttpStatus.OK).body(postService.getPostsByUsername(username));
+    }
 
 }

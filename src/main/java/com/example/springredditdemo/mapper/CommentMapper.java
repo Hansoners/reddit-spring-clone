7ce164ea17f07b21762ea0c369a8f53c4980a80a
hansoners <hansonerschen@gmail.com>
2020-03-11 22:26:50 -0400

    Created dto mapper for comments.

--- src/main/java/com/example/springredditdemo/mapper/CommentMapper.java
+++ src/main/java/com/example/springredditdemo/mapper/CommentMapper.java
@@ -1,4 +1,15 @@
 package com.example.springredditdemo.mapper;
 
+import com.example.springredditdemo.dto.CommentsDto;
+import com.example.springredditdemo.model.Comment;
+import com.example.springredditdemo.model.Post;
+import com.example.springredditdemo.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public class CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    Comment map(CommentsDto commentsDto, Post post, User user);
 }

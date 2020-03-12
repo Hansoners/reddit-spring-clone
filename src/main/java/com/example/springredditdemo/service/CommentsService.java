29cd77b8305866eef039b2dfb7273d196c0104ad
hansoners <hansonerschen@gmail.com>
2020-03-11 22:26:41 -0400

    Updated comments service.

--- src/main/java/com/example/springredditdemo/service/CommentsService.java
+++ src/main/java/com/example/springredditdemo/service/CommentsService.java
@@ -1,4 +1,18 @@
 package com.example.springredditdemo.service;
 
+import com.example.springredditdemo.mapper.CommentMapper;
+import com.example.springredditdemo.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
 public class CommentsService {
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+
 }

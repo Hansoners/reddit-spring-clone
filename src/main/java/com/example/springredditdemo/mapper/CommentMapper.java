a35bd54452b403145a0179813a053e53cb496ded
hansoners <hansonerschen@gmail.com>
2020-03-12 22:11:37 -0400

    Completed comment mapper.:

--- src/main/java/com/example/springredditdemo/mapper/CommentMapper.java
+++ src/main/java/com/example/springredditdemo/mapper/CommentMapper.java
@@ -8,8 +8,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public class CommentMapper {
+public interface CommentMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
     Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId()")
+    @Mapping(target = "user", expression = "java(comment.getUser().getUsername()")
+    CommentsDto maptoDto(Comment comment);
 }

8ea925f3eaf2d692693484fde7f1f9d8ce8f9672
hansoners <hansonerschen@gmail.com>
2020-03-20 22:39:54 -0400

    Added VoteDto

--- src/main/java/com/example/springredditdemo/dto/VoteDto.java
+++ src/main/java/com/example/springredditdemo/dto/VoteDto.java
@@ -1,4 +1,14 @@
 package com.example.springredditdemo.dto;
 
+import com.example.springredditdemo.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VoteDto {
+    private VoteType voteType;
+    private long postId;
 }

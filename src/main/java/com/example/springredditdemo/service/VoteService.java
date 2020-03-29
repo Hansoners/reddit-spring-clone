854f79b5d31aea2d63bc91762dae71fadad83d79
hansoners <hansonerschen@gmail.com>
2020-03-29 15:46:24 -0400

    Modified vote service.

--- src/main/java/com/example/springredditdemo/service/VoteService.java
+++ src/main/java/com/example/springredditdemo/service/VoteService.java
@@ -1,4 +1,20 @@
 package com.example.springredditdemo.service;
 
+import com.example.springredditdemo.repository.PostRepository;
+import com.example.springredditdemo.repository.VotingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
 public class VoteService {
+
+    private final VotingRepository voteRepository;
+    private final PostRepository postRepository;
+    private final AuthService authService;
+
+    @Transactional
+
+
 }
